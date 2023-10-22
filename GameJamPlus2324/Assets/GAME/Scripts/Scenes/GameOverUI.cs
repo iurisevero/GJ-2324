fda13ea8442813e85ea588cdf1a1a66a3f183c41
Jefferson Fillipe <jeffersonfillipe7@gmail.com>
2023-10-22 15:02:17 -0300

    Updated models, other scenes and ui.

--- GameJamPlus2324/Assets/GAME/Scripts/Scenes/GameOverUI.cs
+++ GameJamPlus2324/Assets/GAME/Scripts/Scenes/GameOverUI.cs
@@ -5,10 +5,13 @@ using UnityEngine.UI;
 public class GameOverUI : MonoBehaviour
 {
     [SerializeField] private Button mainMenuButton;
+    [SerializeField] private Button exitButton;
+
     public string mainMenuScene;
 
     void Start()
     {
         mainMenuButton.onClick.AddListener(() => SceneManager.LoadScene(mainMenuScene));
+        exitButton.onClick.AddListener(Application.Quit);
     }
 }
